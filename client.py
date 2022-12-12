0a80a8e2b7a35cc7fd540f7d6749ee5c4620ad21
lucas.fanech <fanech.lucas@gmail.com>
2022-12-12 15:54:07 +0100

    client/server threads; commands ask/verify/cancel/leave

--- client.py
+++ client.py
@@ -1,12 +1,38 @@
-# client.py
+# coding: utf-8
+
 import socket
 
-HOST = '127.0.0.1'  # Adresse IP du serveur
-PORT = 9000      # Port d'écoute du serveur
+# Crée une connexion socket avec le seveur en localhost sur le port 1111
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 1111))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Bonjour serveur !')
-    data = s.recv(1024)
+# demande un numéro d'identification au client via la fonction input()
+r = input("Entrez votre numéro d'identification: ")
+# envoie le numéro d'identification au serveur
+clientsocket.send(r.encode())
+# affiche la réponse du serveur
+response = clientsocket.recv(2048).decode()
+print(response)
+# Si la réponse est "Vous êtes connecté" alors on affiche "Connexion réussie" et on attend une entrée utilisateur infinie
+if response == "Vous êtes connecté":
+    print("Connexion réussie")
+    while True:
+        userInput = input("Entrée utilisateur: ")
+        # envoie l'entrée utilisateur au serveur
+        print("Envoi de l'entrée utilisateur au serveur...")
+        clientsocket.send(userInput.encode())
+        # Si la réponse est "/server-stop" alors on affiche "Connexion terminée" et on ferme la connexion
+        if userInput == "/leave":
+            print("Connexion terminée")
+            clientsocket.close()
+            break
 
-print('Reçu du serveur:', repr(data))
+
+# lorsque l'on ferme la fenêtre, on ferme la connexion
+clientsocket.close()
+
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,17 +1,65 @@
-# serveur.py
 import socket
+import threading
 
-HOST = '127.0.0.1'  # Adresse IP du serveur
-PORT = 9000        # Port d'écoute du serveur
+#fonction qui affiche les threads de la liste threads et les sépare par des virgules
+def print_threads():
+    print("Threads actifs: ", end="")
+    for thread in threads:
+        print(thread, end=", ")
+    print()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connecté par', addr)
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, ip, port, clientsocket):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.clientsocket = clientsocket
+        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port,))
+
+    def run(self):
+        print("Connexion de %s %s" % (self.ip, self.port,))
+
+        r = self.clientsocket.recv(2048)
+        print("Connexion du client n°: ", r.decode(), "...")
+        self.idClient = r.decode()
+        # ajoute le numéro d'identification du client à la liste des threads
+        threads.append(r.decode())
+        # affiche la liste des threads
+        print_threads()
+
+        self.clientsocket.send("Vous êtes connecté".encode())
+        # écoute les messages du client et les affiche
         while True:
-            data = conn.recv(1024)
-            if not data:
+            r = self.clientsocket.recv(2048)
+            print("Client "+self.idClient+" :", r.decode())
+            # Si la réponse est "/call" alors on affiche "Appel en cours"
+            if r.decode() == "/ask":
+                print("Appel pour une question")
+            if r.decode() == "/verify":
+                print("Appel pour une vérification")
+            if r.decode() == "/cancel":
+                print("Annule l'appel")
+
+
+            if r.decode() == "/leave":
+                print("Connexion terminée")
+                self.clientsocket.close()
                 break
-            conn.sendall(data)
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind(("", 1111))
+
+# crée un tableau de threads
+threads = []
+
+while True:
+    tcpsock.listen(10)
+    print("En écoute...")
+    (clientsocket, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port, clientsocket)
+    newthread.start()
+
+
